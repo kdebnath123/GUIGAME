f5c845ac2b1e4b9e331b6bfa3292f5d83a9254d3
kdebnath123 <kirin.debnath@menloschool.org>
2024-02-13 10:00:45 -0800

    worked on dice

--- src/Die.java
+++ src/Die.java
@@ -1,36 +1,51 @@
+import javax.swing.*;
 import java.awt.*;
 
 public class Die {
     /** Instance Variables **/
 
-    private int numSides;
-
+    public static final int NUM_SIDES = 6;
+    public static final int DICE_SIZE = 50;
     private Image[] diceImage;
+
+    private int currentRoll;
+
+
 
     /** Constructors **/
     public Die() {
-        this.numSides = 6;
-        diceImage = new Image[numSides];
+        diceImage = new Image[NUM_SIDES];
 
-        // TODO: import the images
+        currentRoll = -1;
+
+        for(int i = 0; i < NUM_SIDES; i++){
+            diceImage[i] = new ImageIcon("Resources/dice/" + (i + 1) +".png").getImage();
+        }
+
+
+    }
+
+    public  void draw(Graphics g, DiceGameView d){
+
+        for (int i = 0; i< NUM_SIDES; i++){
+            g.drawImage(diceImage[i], 400 + DICE_SIZE*i, 400 + DICE_SIZE*i, d);
+        }
 
     }
 
     /** Methods **/
 
     /**
-     * Returns the number of sides on the Die.
-     */
-    public int getSides() {
-        return numSides;
-    }
-
-    /**
      * Returns a random int between 1 and
      * the number of sides on the Die
      */
     public int roll() {
-        return (int)(Math.random() * (numSides) + 1);
+        currentRoll = (int)(Math.random() * (NUM_SIDES) + 1);
+        return currentRoll;
+    }
+
+    public int getCurrentRoll(){
+        return currentRoll;
     }
 
     /**
@@ -56,6 +71,6 @@ public class Die {
      * "This is an n-sided die."
      */
     public String toString() {
-        return "This is a " + numSides + "-sided die";
+        return "This is a " + NUM_SIDES + "-sided die";
     }
 }
