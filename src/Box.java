b04fba30e7bfe7f3436e8d422db116a64fe5bf4d
kdebnath123 <kirin.debnath@menloschool.org>
2024-02-16 09:49:38 -0800

    worked on dice

--- src/Box.java
+++ src/Box.java
@@ -3,44 +3,38 @@ import java.awt.*;
 
 public class Box {
 
+    /** Box size **/
     public static final int BOX_HEIGHT = 150;
     public static final int BOX_WIDTH = 100;
 
+    /** Box number and open status **/
     private int number;
     private boolean isOpen;
 
+    /** Corner of current box **/
     private int Xcorner;
     private int Ycorner;
 
+    /** Images for open/closed boxes **/
     private Image openImage, closedImage;
 
-    public Box(int number){
+    /** Constructor **/
+    public Box(int number) {
+        // Sets number and starts with open box
         this.number = number;
         this.isOpen = true;
 
+        // Creates lined-up boxes
         Xcorner = DiceGameView.X_OFFSET + BOX_WIDTH*(number - 1);
         Ycorner = DiceGameView.Y_OFFSET;
 
+        // Loads in images
         closedImage = new ImageIcon("Resources/boxes/0.png").getImage();
         openImage = new ImageIcon("Resources/boxes/" + number + ".png").getImage();
     }
 
-    public int getNumber() {
-        return number;
-    }
-
-    public boolean isOpen() {
-        return isOpen;
-    }
-
-    public void close() {
-        isOpen = false;
-    }
-    public void open(){
-        isOpen = true;
-    }
-
-    public  void draw(Graphics g, DiceGameView d){
+    /** Draws box in opened or closed state **/
+    public  void draw(Graphics g, DiceGameView d) {
 
         if(isOpen){
             g.drawImage(openImage, Xcorner, Ycorner, d);
@@ -48,7 +42,19 @@ public class Box {
         else{
             g.drawImage(closedImage, Xcorner, Ycorner + BOX_HEIGHT, d);
         }
-
     }
 
+    /** Getters **/
+    public int getNumber() {
+        return number;
+    }
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+
+    /** Setter **/
+    public void close() {
+        isOpen = false;
+    }
 }
--- src/DiceGameView.java
+++ src/DiceGameView.java
@@ -1,73 +1,79 @@
-import javax.sound.sampled.Line;
 import javax.swing.*;
 import java.awt.*;
 
 public class DiceGameView extends JFrame {
 
+    /** Window attributes **/
     public static final int HEIGHT = 800, WIDTH = 1000,
                             X_OFFSET = 50, Y_OFFSET = 75,
-                            LINE_HIEGHT = 25;
-
+                            LINE_HEIGHT = 25;
     public static final String TITLE = "SHUT THE BOX";
 
+    /** Background image **/
     private Image background;
 
+    /** Shared data **/
     private DiceGame d;
     private Box[] boxes;
     private Die diceOne, diceTwo;
 
+    /** Constructor **/
+    public DiceGameView(DiceGame d) {
 
-    public DiceGameView(DiceGame d){
-
+        // Get need data from front end
         this.d = d;
         boxes = d.getBoxes();
         this.diceOne = d.getDiceOne();
         this.diceTwo = d.getDiceTwo();
 
+        // Loads background image
         background = new ImageIcon("Resources/background.png").getImage();
 
+        // Initializes window
         this.setSize(WIDTH, HEIGHT);
         this.setTitle(TITLE);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
     }
 
+    /** Paints window **/
     public void paint(Graphics g) {
 
-
+        // Resets background
         g.setColor(Color.WHITE);
         g.drawImage(background, 0, 0, this);
 
         // Draws instructions once
         if(d.isFirstTime()) {
             g.drawString("Welcome to close the box.", X_OFFSET, Y_OFFSET );
-            g.drawString("Each turn roll 2 dice, and sum the numbers.", X_OFFSET, Y_OFFSET + LINE_HIEGHT);
-            g.drawString("Then 'shut' the numbers until it adds up the same sum.", X_OFFSET, Y_OFFSET + 2*LINE_HIEGHT);
-            g.drawString("Try to get the lowest score.", X_OFFSET, Y_OFFSET + 3*LINE_HIEGHT);
-            g.drawString("Input to confirm.", X_OFFSET, Y_OFFSET + 4* LINE_HIEGHT);
+            g.drawString("Each turn roll 2 dice, and sum the numbers.", X_OFFSET, Y_OFFSET + LINE_HEIGHT);
+            g.drawString("Then 'shut' any combination of boxes that adds to the sum.", X_OFFSET, Y_OFFSET + 2* LINE_HEIGHT);
+            g.drawString("Try to get the lowest score.", X_OFFSET, Y_OFFSET + 3* LINE_HEIGHT);
+            g.drawString("Input to confirm.", X_OFFSET, Y_OFFSET + 4* LINE_HEIGHT);
             return;
         }
 
-       diceOne.draw(g, this);
-       diceTwo.draw(g, this);
+        // Draw dice
+        diceOne.draw(g, this);
+        diceTwo.draw(g, this);
 
+        // Inform user of sum and score
+        g.drawString("SUM: "+ d.getSum(), X_OFFSET, HEIGHT - Y_OFFSET + LINE_HEIGHT);
+        g.drawString("SCORE: "+ d.getScore(), WIDTH - 3*X_OFFSET, HEIGHT - Y_OFFSET + LINE_HEIGHT);
 
-       g.drawString("SUM: "+ d.getSum(), X_OFFSET, HEIGHT - Y_OFFSET + LINE_HIEGHT);
-       g.drawString("SCORE: "+ d.getScore(), WIDTH - 3*X_OFFSET, HEIGHT - Y_OFFSET + LINE_HIEGHT);
-
-
+        // Draw each box
         for (Box b: boxes) {
             b.draw(g, this);
         }
 
+        // Won/Loss screen
         if(d.hasWon()) {
             g.drawString("YOU CLEARED THE BOARD GOOD JOB!", X_OFFSET, Y_OFFSET);
-            g.drawString("YOU WON!!!!", X_OFFSET, Y_OFFSET + LINE_HIEGHT);
+            g.drawString("YOU WON!!!!", X_OFFSET, Y_OFFSET + LINE_HEIGHT);
         }
-
         if(d.hasLossed()) {
-            g.drawString("No More Possible moves :(", (int)(WIDTH / 3.0), HEIGHT - Y_OFFSET + LINE_HIEGHT);
-            g.drawString("FINAL ", WIDTH - 4*(X_OFFSET - 1), HEIGHT - Y_OFFSET + LINE_HIEGHT);
+            g.drawString("No More Possible moves :(", (int)(WIDTH / 3.0), HEIGHT - Y_OFFSET + LINE_HEIGHT);
+            g.drawString("FINAL ", WIDTH - 4*(X_OFFSET - 1), HEIGHT - Y_OFFSET + LINE_HEIGHT);
         }
     }
 }
